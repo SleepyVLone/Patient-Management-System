7bce42baba2b721a5f79514540bf50df4b0a11b6
SleepyVLone <sleepycb17@gmail.com>
2026-03-31 04:21:39 +0100

    v1.0.6 - Replace std::hash with FNV-1a for cross-platform consistency

--- hashPassword.cpp
+++ hashPassword.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <functional>
 #include <string>
+#include <cstdint>
 
 #include "hashPassword.h"
 
@@ -8,7 +8,16 @@ using namespace std;
 
 string hashPassword(const string& password)
 {
-    hash<string> hasher;
-    size_t hashValue = hasher(password);    //Generates a hash value from the password provided by the user
-    return to_string(hashValue);            //Converts the hash to a string
+    const uint64_t FNV_OFFSET_BASIS = 14695981039346656037ULL;
+    const uint64_t FNV_PRIME = 1099511628211ULL;
+
+    uint64_t hashValue = FNV_OFFSET_BASIS;
+
+    for (unsigned char byte : password)
+    {
+        hashValue ^= byte;           //Take each character from the password and mix it into the hash value using XOR
+        hashValue *= FNV_PRIME;      //Mix the hash bits more by multiplying with the FNV prime
+    }
+
+    return to_string(hashValue);     // Converts the hash to a string
 }
